bcc6efd07cbd0279431b284d90385ca92d53136e
Quan Huyn <hhq851999@gmail.com>
2026-03-13 22:14:09 +0700

    feat: import morgan, compression and helmet, config root router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+// Middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// router
+app.use("/", require("./routes/index"));
 
 module.exports = app;
